ce5b32c4cee245dc0b071a80f4f99b6c0adb8fbd
Arsh Bahndari <arshbhandari39@gmail.com>
2026-02-24 23:18:45 +0530

    Redux toolkit project

---
+++ 11reduxToolkit/src/components/Todo.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useSelector ,useDispatch } from 'react-redux'
+import { removeTodo } from '../features/todo/todo'
+
+function Todo(){
+    const todos = useSelector(state => state.todos)
+    const dispatch = useDispatch()
+  return (
+    <>
+      <div>Todos</div>
+      {todos.map((todo)=>(
+        <li key ={todo.id}>
+            {todo.text}
+            <button onClick={()=> dispatch(removeTodo(todo.id))}>X</button>
+        </li>
+        
+      ))}
+    </>
+  )
+}
+
+export default Todo
